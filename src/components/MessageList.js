ef5146359aeaea2b5bfe38d9016419fe84a27d5d
David Allen <david@davidnallen.com>
2018-06-27 13:17:48 -0600

    Moving index and style to public

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,10 +1,32 @@
 import React from 'react'
 
+const DUMMY_DATA = [
+    {
+        senderId: 'perborgen',
+        text: 'Hey, how is it going?'
+    },
+    {
+        senderId: 'janedoe',
+        text: 'Great! How about you?'
+    },
+    {
+        senderId: 'perborgen',
+        text: 'Good to hear! I am great as well'
+    }
+]
+
 class MessageList extends React.Component {
     render() {
         return (
             <div className="message-list">
-                <div className="help-text">MessageList</div>
+                {DUMMY_DATA.map((message, index) => {
+                    return (
+                        <div>
+                        <div>{message.senderId}</div>
+                        <div>{message.text}</div>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
