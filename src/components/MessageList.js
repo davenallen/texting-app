9d9c2b0c463e1ddbe0c5adfddbbb12cea0a8c126
David Allen <david@davidnallen.com>
2018-06-27 20:07:01 -0600

    Chat Rooms Scrolling Freature

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,30 +1,27 @@
 import React from 'react'
-
-const DUMMY_DATA = [
-    {
-        senderId: 'perborgen',
-        text: 'Hey, how is it going?'
-    },
-    {
-        senderId: 'janedoe',
-        text: 'Great! How about you?'
-    },
-    {
-        senderId: 'perborgen',
-        text: 'Good to hear! I am great as well'
-    }
-]
+import ReactDOM from 'react-dom'
+import Message from './Message'
 
 class MessageList extends React.Component {
+
+  componentWillUpdate() {
+        const node = ReactDOM.findDOMNode(this)
+        this.shouldScrollToBottom = node.scrollTop + node.clientHeight + 100 >= node.scrollHeight
+    }
+
+    componentDidUpdate() {
+        if (this.shouldScrollToBottom) {
+            const node = ReactDOM.findDOMNode(this)
+            node.scrollTop = node.scrollHeight
+        }
+    }
+
     render() {
         return (
             <div className="message-list">
-                {DUMMY_DATA.map((message, index) => {
+                {this.props.messages.map((message, index) => {
                     return (
-                        <div>
-                        <div>{message.senderId}</div>
-                        <div>{message.text}</div>
-                        </div>
+                      <Message key={index} username={message.senderId} text={message.text} />
                     )
                 })}
             </div>
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/af799227-05d3-4a5b-ae27-8e378f5683b1/token";
-const instanceLocator = "v1:us1:af799227-05d3-4a5b-ae27-8e378f5683b1";
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/cfff6620-bf16-4278-a8d9-e32341399d5a/token";
+const instanceLocator = "v1:us1:cfff6620-bf16-4278-a8d9-e32341399d5a";
 
 exports.tokenUrl = tokenUrl;
 exports.instanceLocator = instanceLocator;
